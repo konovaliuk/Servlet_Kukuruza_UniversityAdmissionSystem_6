7ee4b7de2f76beca9f01485c9f8a529a3b29ac1a
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2019-08-31 02:06:40 +0300

    implemented basic features for UserExam table

---
+++ src/main/java/ua/epam/training/kukuruza/model/dao/IDaoUserExam.java
@@ -0,0 +1,6 @@
+package ua.epam.training.kukuruza.model.dao;
+
+import ua.epam.training.kukuruza.model.entity.UserExam;
+
+public interface IDaoUserExam extends IDaoGeneric<UserExam, Long> {
+}
---
+++ src/main/java/ua/epam/training/kukuruza/model/dao/mapper/UserExamMapper.java
@@ -0,0 +1,29 @@
+package ua.epam.training.kukuruza.model.dao.mapper;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ua.epam.training.kukuruza.model.entity.UserExam;
+import ua.epam.training.kukuruza.model.exception.MapperException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserExamMapper {
+    private static final Logger LOGGER = LogManager.getLogger(UserExamMapper.class);
+    private static final String ID = "id";
+    private static final String USER_ID = "user_id";
+    private static final String EXAM_ID = "exam_id";
+
+    public static UserExam map(ResultSet rs) {
+        UserExam.Builder builder = new UserExam.Builder();
+        try {
+            builder.setId(rs.getLong(ID))
+                    .setUserId(rs.getLong(USER_ID))
+                    .setExamId(rs.getInt(EXAM_ID));
+            return builder.build();
+        } catch (SQLException e) {
+            LOGGER.error("Can't map ResultSet to UserExam", e);
+            throw new MapperException(e);
+        }
+    }
+}

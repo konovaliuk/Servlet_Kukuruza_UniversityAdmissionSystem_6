2e222b6281e4f561231fb4f239fa40ddf36eceb7
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2019-09-12 14:08:13 +0300

    added and implemented features

--- src/main/java/ua/epam/training/kukuruza/model/dao/IDaoUserExam.java
+++ src/main/java/ua/epam/training/kukuruza/model/dao/IDaoUserExam.java
@@ -2,8 +2,10 @@ package ua.epam.training.kukuruza.model.dao;
 
 import ua.epam.training.kukuruza.model.entity.UserExam;
 
+import java.util.List;
 import java.util.Set;
 
 public interface IDaoUserExam extends IDaoGeneric<UserExam, Long> {
     Set<Integer> getExamsIdByUserId(Long userId);
+    void save(List<UserExam> userExams);
 }
